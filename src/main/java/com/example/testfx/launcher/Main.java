1e496346ef9a891d42a813e96062c5b2bbc8df2f
clément <clement.vazeille@gmail.com>
2023-08-05 18:24:56 +0200

    version test

--- src/main/java/com/example/testfx/launcher/Main.java
+++ src/main/java/com/example/testfx/launcher/Main.java
@@ -1,8 +1,11 @@
 package com.example.testfx.launcher;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
@@ -19,6 +22,8 @@ public class Main extends Application {
         launch(args);   //launch start method
     }
 
+    Button button_left;
+    Button button_right;
     @Override
     public void start(Stage stage) throws IOException {
         //create a scene and set it to the stage
@@ -46,8 +51,41 @@ public class Main extends Application {
         line.setEndY(45);
         line.setStrokeWidth(2);
 
+        button_left = new Button();
+        button_left.setText("Precedent Month");
+        button_left.setLayoutY(200);
+        button_left.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Left");
+                Text textadded = new Text();
+                textadded.setText("Sheeeesh");
+                textadded.setX(80);
+                textadded.setY(200);
+                root.getChildren().add(textadded);
+            }
+        });
+
+        button_right = new Button();
+        button_right.setText("Next Month");
+        button_right.setLayoutY(200);
+        button_right.setLayoutX(540);
+        button_right.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("right");
+                Text textadded = new Text();
+                textadded.setText("Sheeeesh");
+                textadded.setX(500);
+                textadded.setY(200);
+                root.getChildren().add(textadded);
+            }
+        });
+
         root.getChildren().add(text);
         root.getChildren().add(line);
+        root.getChildren().add(button_left);
+        root.getChildren().add(button_right);
 
         stage.show();
     }
